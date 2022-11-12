485f3b7c87d85bfa191d203b8af284588804d58c
browynlouis <browynlouis2@gmail.com>
2022-11-12 20:30:34 +0100

    component: created on NavBar component

---
+++ src/components/Layout/Partials/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const List = styled.ul`
+  display: flex;
+  gap: ${(props) => props.theme.spacing(6)};
+  list-style: none;
+
+  li {
+    a {
+      font-size: 20px;
+      color: ${(props) => props.theme.pallete.black};
+    }
+  }
+`;
+
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <List>
+          <li>
+            <a href="/">Home </a>
+          </li>
+          <li>
+            <a href="/">Place to stay </a>
+          </li>
+          <li>
+            <a href="/">NFTs </a>
+          </li>
+          <li>
+            <a href="/">Community </a>
+          </li>
+        </List>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
